c97f3b35ced92b6d8e902aaf86f6fad2116ac3a3
JoseFaccirolli <jose.faccirolli08@gmail.com>
2026-05-17 00:35:26 -0300

    (insertQueryGenerator.py)V2: Adicionando a possibilidade de um atributo ter um prefixo (bom para cpf)

--- pessoais/insertQueryGenerator.py
+++ pessoais/insertQueryGenerator.py
@@ -23,12 +23,31 @@ def insertQueryGenerator():
   for i in range(attributes):
     arguments.append(input(f"Enter the {i+1}º element of the table: "))
 
+  prefix = str(input("Does any attribute have a prefix? (y/N): "))
+  
+  lenOfCount = 1
+  prefixPlace = -1
+  if (prefix.lower() == "y"):
+    print("Wich position has a prefix:")
+    for i in range(attributes):
+      print(f"{i+1}. [{arguments[i]}]")
+    prefixPlace = int(input("Enter a number: "))
+    prefixPlace -= 1
+
+    prefix = str(input("What is the prefix: "))
+    lenOfCount = int(input("Enter the size of the sufix (number of digits): "))
+    if lenOfCount < 1: lenOfCount = 1
+
   # Values structure
-  count = 0
+  count = 10 ** (lenOfCount - 1)
+
   for i in range(numberOfinserts):
     values = []
     for j in range(attributes):
-      values.append("'" + str(count))
+      if prefixPlace == j:
+        values.append("'" + prefix + str(count))
+      else:
+        values.append("'" + str(count))
 
     # inside the "()" in values
     places = "', ".join(values) + "'"
